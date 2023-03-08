2fb3a319d8b9e6e6f2ec9fc8eac3cf36d0a43f53
fbakalov <114005524+fbakalov@users.noreply.github.com>
2023-03-08 21:34:54 +0200

    Added ExceptionsAndErrorHandling lecture only 2 exercises

---
+++ OOP/OOP_ExceptionsAndErrorHandling/01. Square Root/Program.cs
@@ -0,0 +1,18 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+try
+{
+	if (number < 0)
+	{
+		throw new Exception("Invalid number.");
+	}
+    Console.WriteLine(Math.Sqrt(number));
+}
+catch (Exception ex)
+{
+	Console.WriteLine(ex.Message);
+}
+finally
+{
+	Console.WriteLine("Goodbye.");
+}
---
+++ OOP/OOP_ExceptionsAndErrorHandling/02. Enter Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine(string.Join(", ", ReadNumber(1, 100))); 
+
+List<int> ReadNumber(int start, int end)
+{
+    List<int> result = new();
+    int count = 0;
+	while (count != 10)
+	{
+		try
+		{
+            int currentNum = int.Parse(Console.ReadLine());
+
+            if (currentNum <= start || currentNum >= end)
+            {
+                throw new ArgumentException($"Your number is not in range ({currentNum} - {end}!)");
+            }
+            else
+            {
+                result.Add(currentNum);
+                count++;
+            }    
+        }
+		catch (FormatException ex)
+		{
+			Console.WriteLine("Invalid Number!");
+		}
+        catch(ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+	}
+
+    return result;
+}
